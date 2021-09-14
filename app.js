2388cd52e2d5b6991592bc973c0c15d2dc89616d
Bobby Keel <bobbykeel76@gmail.com>
2021-09-14 14:01:08 -0500

    configured with multer to accept file uploads using uploadRouter, and then added to app.js the route

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter');
+
 // third party module
 // create schemas and manipulate (CreateReadUpdateDelete) our mongoDB database
 // without this middleware our express applicaiton cannot interact with out mongodb
@@ -19,6 +21,7 @@ const partnerRouter = require('./routes/partnerRouter');
 //also without this you would have to manualy manipulate mongodb which was showcased 
 // during nodejs driver exercise
 const mongoose = require('mongoose');
+const { updateOne } = require('./models/user');
 
 // decalring variable with name of our database
 //in this case my database is "nucampsite"
@@ -90,6 +93,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler 
 // uses http-errors when user goes to url that does not exist in code
---
+++ routes/uploadRouter.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const authenticate = require('../authenticate'); 
+// import multer = proccesses multipart/form data enctype
+// built on top of busboy another middleware library for processing incoming data
+// multer adds 2 parts to req object file/files and body objects
+// body contains text form any text fields
+// file/files contain file or files
+const multer = require('multer'); 
+
+// config for how multer will handle file uploads
+// multer has default values for this too
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => { // cb is callback funct
+    cb(null, 'public/images'); // (error, save directory)
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname); // (error, sets name in server same as name from client otherwise will give random string)
+  }
+});
+
+const imageFileFilter = (req, file, cb) => { // filter to evaluate the file
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) { // regex to check if file is image file ext
+    return cb(new Error('You can upload only image files!'), false); // returns this error if not image file ext
+  }
+  cb(null, true); // (error, accept file)
+};
+
+// call multer funstion
+const upload = multer({storage: storage, fileFilter: imageFileFilter});
+
+const uploadRouter = express.Router(); // create uplaodRouter
+
+// configure upload router to handle various request
+uploadRouter.route('/')
+.get(authenticate.verifyUser, authenticate.verifyAdmin, (req,res) => {
+  res.statusCode = 403;
+  res.end('GET operation not supported on /imageUpload');
+})
+// when client uploads a file to server multer will take over and handle any errors, after complete and returns with no errors that means file was succeffully uploaded to server
+.post(authenticate.verifyUser, authenticate.verifyAdmin, upload.single('imageFile'), (req,res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json(req.file); // confirms to client the file was uploaded succeffully
+})
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req,res) => {
+  res.statusCode = 403;
+  res.end('PUT operation not supported on /imageUpload');
+})
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req,res) => {
+  res.statusCode = 403;
+  res.end('DELETE operation not supported on /imageUpload');
+});
+
+module.exports = uploadRouter; // export uploadrouter
